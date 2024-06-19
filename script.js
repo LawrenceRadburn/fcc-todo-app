40d872454e48d8c0e86f6360a48c64fd07dc27cf
Lawrence Radburn <lawrence@radnet.ca>
2024-06-18 21:20:56 -0400

    feat: get taskObject value to console

--- script.js
+++ script.js
@@ -32,6 +32,15 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+    const taskObj = {
+        id: titleInput.value.toLowerCase()
+
+        };
+    console.log(taskObj);
 });
 
-const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+
