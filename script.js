8604143b25190698005dd0e1930f1fc604e2b347
Lawrence Radburn <lawrence@radnet.ca>
2024-06-21 21:38:37 -0400

    feat: Title embedded expression

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ taskForm.addEventListener("submit", (e) => {
 
         taskData.forEach(({ id, title, date, description }) => {
             tasksContainer.innerHTML += `
-            <div class="task" id="${id}"</div>`;
+            <div class="task" id="${id}"</div>
+            <p><strong>Title:</strong>${title}</p>`;
         });
 });
